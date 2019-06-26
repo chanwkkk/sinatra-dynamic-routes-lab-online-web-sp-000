f8d77b016b36d850433c2c6c2349aced5eab1b76
Yingqi Chen <chanwk18@hotmail.com>
2019-06-26 20:54:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,4 +22,10 @@ class App < Sinatra::Base
     "#{params[:word1]+' '+params[:word2]+' '+params[:word3]+' '+params[:word4]+' '+params[:word5]}"
   end
 
+  get '/:operation/:number1/:number2' do
+    @operation=params[:operation]
+    @num1=params[:number1]
+    @num2=params[:number2]
+  end
+
 end
