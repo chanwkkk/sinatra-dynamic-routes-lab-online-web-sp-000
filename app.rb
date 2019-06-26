a9f35855e0a6011098142d376f0df555f1792aa8
Yingqi Chen <chanwk18@hotmail.com>
2019-06-26 20:39:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num=params[:number].to_i
     @phrase=params[:phrase]
-    @num.times {puts "#{@phrase}"}
+    @num.times {puts "#{@phrase}"}.to_s
   end
 
 end
