0e5aaef37a1f745f90cdb77ca657ff4c89f1f636
Yingqi Chen <chanwk18@hotmail.com>
2019-06-26 20:47:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,4 +18,8 @@ class App < Sinatra::Base
     erb :phrase
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] params[:word2] params[:word3] params[:word4] params[:word5]}"
+  end
+
 end
