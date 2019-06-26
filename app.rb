ddccf12d813f0adbb163e2d9203251208dceb225
Yingqi Chen <chanwk18@hotmail.com>
2019-06-26 21:00:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,8 +12,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @num=params[:number].to_i
-    @phrase=params[:phrase]
+    num=params[:number].to_i
+    phrase=params[:phrase]
 
     erb :phrase
   end
